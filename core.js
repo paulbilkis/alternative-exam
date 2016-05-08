744c26777001011f3f2d52ece73d782680c33ac6
paulbilkis <jew.bilkis@gmail.com>
2016-05-09 01:06:00 +0300

    added gen_yung_table(); that gen a <table> from brackets

--- core.js
+++ core.js
@@ -1,7 +1,7 @@
-var canvas = document.createElement("canvas");
+/*var canvas = document.createElement("canvas");
     ctx = canvas.getContext("2d");
     canvas.width = 300;
     canvas.height = 300;
-document.body.appendChild(canvas);
+document.body.appendChild(canvas);*/
 
-draw_a_monoway ([1,0,1,1,0,0,1,0,1,1,0,0]);
+gen_yung_table ([1,0,1,0,1,0, 1,1,1,0,0,0], document.body);
